b00600097e2eb960c81526c390d6bedb38ea62e7
Raghunadh <Raghunadh@RAGHU>
2020-02-17 15:26:53 +0530

    Post Man Rest Controller with Spring MVC first commit

---
+++ src/test/java/springbootstarter/topic/Rest/api/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootstarter.topic.Rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
