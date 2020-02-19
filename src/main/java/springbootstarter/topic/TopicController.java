08d3f2bb3e58f0a980dc331ddc0838b93fcc78dd
Raghunadh <Raghunadh@RAGHU>
2020-02-19 13:54:17 +0530

    controller

--- src/main/java/springbootstarter/topic/TopicController.java
+++ src/main/java/springbootstarter/topic/TopicController.java
@@ -1,5 +1,16 @@
 package springbootstarter.topic;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TopicController {
 
-}//class
+	String name = "My Name is: Raghu";
+
+	@RequestMapping("/name")
+	public String getName() {
+		return name;
+	}
+
+}// class
